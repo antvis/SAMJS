960962ba169704e21053d6dab19d15d19405e104
yzg1025 <yzg862689234@163.com>
2023-05-19 18:53:36 +0800

    feat: 图片裁切添加框选mask

--- docs/typing.ts
+++ docs/typing.ts
@@ -1,6 +1,6 @@
 import { ILayer, ISource, Scene } from '@antv/l7';
 // @ts-ignore
-import { SAMGeo } from 'sam.js';
+import { SAM, SAMGeo } from 'sam.js';
 
 export interface SatelliteData {
   features: any;
@@ -17,4 +17,24 @@ export interface ISamState {
   eventType: string;
   collapsed: boolean;
   satelliteData: SatelliteData[];
+}
+
+// 像素坐标
+export type CoordsPS = {
+  x: number;
+  y: number;
+};
+
+export interface ISamStateImg {
+  imageUrl: string;
+  samModel: SAM | null;
+  loading: boolean;
+  clipImg: string[];
+  originImg: HTMLImageElement | null;
+  originMaskImg: string[];
+  isSelecting: boolean;
+  startPoint: CoordsPS;
+  endPoint: CoordsPS;
+  clipType: string;
+  eventPoint: number[] | number[][];
 }
