b63cb91ae7d194fcf618160bf1a8310767851bc6
yzg1025 <yzg862689234@163.com>
2023-05-29 19:44:02 +0800

    chore: 修改问题

--- docs/components/normal-map/index.tsx
+++ docs/components/normal-map/index.tsx
@@ -1,26 +1,21 @@
-import {
-  Button,
-  Form,
-  Input,
-  message,
-  Radio,
-  Slider,
-  Spin,
-  Upload,
-} from 'antd';
-import React, { useEffect, useState } from 'react';
+import { Button, Form, Input, message, Radio, Spin, Upload } from 'antd';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { UploadOutlined } from '@ant-design/icons';
 // @ts-ignore
 import { getBase64, image2Base64, SAMGeo } from '@antv/sam';
+import { useSize } from 'ahooks';
 import { EMBEDDING_URL } from '../../config';
 import { SatelliteData } from '../../typing';
+import { getAbsoluteXy } from '../../utils';
 import { Model_URL, WasmPaths } from '../constant';
 import { locations } from './constant';
 import './index.less';
 import { RightPanel } from './leftpanel';
 
 export default () => {
+  const imgRef = useRef();
+  const imgSize = useSize(imgRef);
   const [form] = Form.useForm();
   const [loading, setLoading] = useState(false);
   const [samModel, setSamModel] = useState<SAMGeo>(null);
@@ -31,7 +26,6 @@ export default () => {
     (SatelliteData & { maskImg: string; key: string })[]
   >([]);
   const [place, setPlace] = useState(locations[0].name);
-  const [simplifyThreshold, setSimplifyThreshold] = useState(1);
 
   // 生成 embedding 并初始化载入模型
   const embedding = async (base64Url: string) => {
@@ -89,27 +83,13 @@ export default () => {
   }, [samModel, analyzeImg]);
 
   const onMapClick = (e) => {
-    const rect = e.nativeEvent.target.getBoundingClientRect();
-    let x = Math.round(e.pageX - rect.left);
-    let y = Math.round(e.pageY - rect.top);
-    // 获取渲染图片与原图片的缩放比
-    const imageScaleX = originImg
-      ? originImg.width / e.nativeEvent.target.offsetWidth
-      : 1;
-    const imageScaleY = originImg
-      ? originImg.height / e.nativeEvent.target.offsetHeight
-      : 1;
-    x *= imageScaleX;
-    y *= imageScaleY;
+    const { x, y } = getAbsoluteXy(e, originImg);
     const position = [{ x, y, clickType: 1 }];
     samModel.predict(position).then(async (output) => {
       const image = samModel.exportImageClip(output);
       const maskImg = samModel.exportMaskImage(output);
       // 导出 polygon
-      const polygon = await samModel.exportGeoPolygon(
-        output,
-        simplifyThreshold,
-      );
+      const polygon = await samModel.exportGeoPolygon(output, 1);
       const newData = {
         features: polygon.features,
         imageUrl: image.src,
@@ -128,7 +108,6 @@ export default () => {
 
   const onFinish = async (value: Record<string, any>) => {
     try {
-      setSimplifyThreshold(value?.simplifyThreshold ?? 1);
       if (value?.upload) {
         const { upload, coordinate } = value;
         const file = upload[0];
@@ -190,6 +169,7 @@ export default () => {
         onFinish={onFinish}
         labelCol={{ span: 3 }}
         labelAlign="left"
+        className="formLayout"
         onFieldsChange={onFieldsChange}
       >
         <Form.Item name="choose" label="示例">
@@ -236,9 +216,6 @@ export default () => {
             style={{ width: 300 }}
           />
         </Form.Item>
-        <Form.Item label="polygon抽稀系数:" name="simplifyThreshold">
-          <Slider step={0.1} min={0} max={100} style={{ width: 300 }} />
-        </Form.Item>
         <Form.Item>
           <Button type="primary" htmlType="submit">
             提交
@@ -247,14 +224,31 @@ export default () => {
       </Form>
 
       <Spin spinning={loading} tip={'embedding 生成中……'}>
-        <div className="normalMapContainer">
-          <img src={analyzeImg} style={{ width: '68%' }} onClick={onMapClick} />
-          {satelliteData.length
-            ? satelliteData.map((item, index) => (
-                <img key={index} className="normalMaskImg" src={item.maskImg} />
-              ))
-            : null}
+        <div
+          className="normalMapContainer"
+          style={{ height: imgSize?.height ?? 500 }}
+        >
+          <div style={{ width: '68%', position: 'relative' }}>
+            <img
+              ref={imgRef as any}
+              src={analyzeImg}
+              onClick={onMapClick}
+              width="100%"
+              style={{ position: 'absolute', cursor: 'pointer' }}
+            />
+            {satelliteData.length
+              ? satelliteData.map((item, index) => (
+                  <img
+                    key={index}
+                    className="normalMaskImg"
+                    src={item.maskImg}
+                  />
+                ))
+              : null}
+          </div>
+
           <RightPanel
+            height={imgSize?.height ?? 400}
             satelliteData={satelliteData}
             setSatelliteData={setSatelliteData}
           />
